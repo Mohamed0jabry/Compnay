1af4c92c30feee0cecfca9f2e291bf39603b73a3
Mohamed0jabry <mohamedalgabry@gamil.com>
2023-12-30 15:19:55 +0200

    first commit~

---
+++ src/company_products_manager/DBInfo.java
@@ -0,0 +1,18 @@
+
+package company_products_manager;
+/*
+public interface DBInfo {
+    String DB_NAME = "jdbc:mysql://auth-db441.hstgr.io/u609809503_products";
+    String ENCODING = "?useUnicode=yes&characterEncoding=UTF-8";
+    String DB_NAME_WITH_ENCODING = DB_NAME + ENCODING;
+    String USER = "u609809503_products";
+    String PASSWORD = "g7hP~eXk7=";
+}
+*/
+public interface DBInfo {
+    String DB_NAME = "jdbc:mysql://localhost/products_db";
+    String ENCODING = "?useUnicode=yes&characterEncoding=UTF-8";
+    String DB_NAME_WITH_ENCODING = DB_NAME + ENCODING;
+    String USER = "root";
+    String PASSWORD = "";
+}
